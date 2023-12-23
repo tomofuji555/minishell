11a70078368bc70a95af04639ab2ba667c6562f7
Your Name <you@example.com>
2023-12-23 20:19:14 +0900

    .hコミットし忘れ

--- execute.h
+++ execute.h
@@ -6,15 +6,16 @@
 /*   By: toshi <toshi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 18:29:28 by tofujiwa          #+#    #+#             */
-/*   Updated: 2023/12/19 19:36:33 by toshi            ###   ########.fr       */
+/*   Updated: 2023/12/23 17:07:14 by toshi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#ifndef EXECUTE_H
+# define EXECUTE_H
 
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
 
 enum	e_pipefd_direct
 {
@@ -57,31 +58,52 @@ typedef struct s_token
 	struct s_token		*next;
 }	t_token;
 
-typedef struct s_tree_node
-{
-	t_token			*cmd_tokens;
-	t_token			*infile_tokens;
-	t_token			*outfile_tokens;
-	int				last_cmd_flag;
-	struct s_node	*prev;
-	struct s_node	*left;
-	struct s_node	*right;
-}	t_tree_node;
-
 typedef struct s_redir
 {
 	enum e_redir_kind	kind;
 	char				*val;
-	struct s_token		*next;
+	struct s_redir		*next;
 }	t_redir;
 
-typedef struct s_exec_handler
-{
-	t_tree_node		*cur_node;
-	char			**strs;				//execまで使わない
-	t_redir			*infile_paths;		//execまで使わない
-	t_redir			*outfile_paths;		//execまで使わない
-	int				prev_pipe_in_fd;
-}	t_exec_handler;
+// typedef struct s_tree_node
+// {
+// 	t_token			*cmd_tokens;
+// 	t_token			*infile_tokens;
+// 	t_token			*outfile_tokens;
+// 	char			**cmd_args;
+// 	t_redir			*infile_paths;
+// 	t_redir			*outfile_paths;
+// 	struct s_node	*prev;
+// 	struct s_node	*left;
+// 	struct s_node	*right;
+// }	t_tree_node;
+
+// typedef struct s_exec_handler
+// {
+// 	t_tree_node		*cur_node;
+// 	char			**strs;				//execまで使わない
+// 	t_redir			*infile_paths;		//execまで使わない
+// 	t_redir			*outfile_paths;		//execまで使わない
+// 	int				prev_pipe_in_fd;
+// }	t_exec_handler;
+
+
+t_redir	*make_redir_list(t_token *tkn_ptr);
+void	print_token_list(t_token *head);
+t_token *make_token_node(char *str, enum e_token_kind kind);
+t_token *find_last_node(t_token *head);
+void add_node_last(t_token **head, t_token *new);
+t_token	*test_make_token_list(char *strs[], enum e_token_kind kinds[]);
+
+
+t_redir	*make_redir_list(t_token *tkn_ptr);
+int	is_redir_token(enum e_token_kind kind);
+size_t	strlen_of_list_untill_last(t_token *begining, t_token *last);
+size_t	strlcpy_from_list(char *dest, t_token *src, size_t len);
+enum e_redir_kind	convert_redir_kind(enum e_token_kind token_kind);
+t_redir	*make_redir_node(t_token *begining, t_token *last);
+t_redir	*find_last_redir_node(t_redir *head);
+void	add_redir_node_last(t_redir **head_node, t_redir *new_node);
+
 
 #endif
