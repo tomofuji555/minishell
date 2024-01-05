1fcc2e06a8c8741b7d24da0e4914b0217d88a80c
Your Name <you@example.com>
2024-01-05 17:26:46 +0900

    tokenizeに必要な変数と実行に必要な変数をそれぞれ別の構造体に分けた

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: toshi <toshi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 18:29:28 by tofujiwa          #+#    #+#             */
-/*   Updated: 2023/12/09 15:52:58 by toshi            ###   ########.fr       */
+/*   Updated: 2024/01/05 17:24:58 by toshi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 # define MINISHELL_H
 
 #include <stdlib.h>
+
+typedef enum e_bool
+{
+	FALSE	= 0,
+	TRUE	= 1
+}	t_bool;
 
 enum e_token_kind
 {
@@ -36,14 +42,42 @@ typedef struct s_token
 	struct s_token		*next;
 }	t_token;
 
+enum e_redir_kind
+{
+	REDIR_IN_FILE		= 6,
+	REDIR_HERE_DOC		= 7,
+	REDIR_OUT_FILE		= 8,
+	REDIR_APPEND_FILE	= 9,
+};
+
+typedef struct s_redir
+{
+	enum e_redir_kind	kind;
+	char				*val;
+	struct s_redir		*next;
+}	t_redir;
+
+typedef struct s_init_arg_data
+{
+	t_token				*cmd_tokens;
+	t_token				*infile_tokens;
+	t_token				*outfile_tokens;
+}	t_init_arg_data;
+
+typedef struct s_exec_arg_data
+{
+	char				**cmd_args;
+	t_redir				*infile_paths;
+	t_redir				*outfile_paths;
+}	t_exec_arg_data;
+
 typedef struct s_tree_node
 {
-	t_token			*cmd_tokens;
-	t_token			*infile_tokens;
-	t_token			*outfile_tokens;
-	struct s_node	*prev;
-	struct s_node	*left;
-	struct s_node	*right;
+	t_init_arg_data		init_arg_data;
+	t_exec_arg_data		exec_arg_data;
+	struct s_tree_node	*prev;
+	struct s_tree_node	*left;
+	struct s_tree_node	*right;
 }	t_tree_node;
 
 #endif
