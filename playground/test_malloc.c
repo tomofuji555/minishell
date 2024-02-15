4d3f879210eb862df272e7f9795a3da4c49c6177
Your Name <you@example.com>
2024-02-15 11:23:50 +0900

    strerrorの使い方を理解

--- playground/test_malloc.c
+++ playground/test_malloc.c
@@ -3,20 +3,21 @@
 #include <string.h>
 #include <errno.h>
 
-__attribute__((destructor))
-static void destructor() {
-    system("leaks -q a.out");
-}
+// __attribute__((destructor))
+// static void destructor() {
+//     system("leaks -q a.out");
+// }
 
 int main()
 {
 	errno = 0;
 	// size_t i = 4294967295;
-	char *str = (char *)malloc(sizeof(char) * (999999999999999 + 1));
+	char *str = (char *)malloc(sizeof(char) * (9999999999999999999 + 1));
 	if (str == NULL)
 	{
-		//perror("");
-		strerror(errno);
+		perror("lalala");
+		// strerror(errno);
+		printf("%s\n", strerror(errno));
 	}
 	else
 	{
