018763cd92419ec2f6a5661baf38d6bb297c1f29
tozeki <toshi326etu.sera@gmail.com>
2024-04-28 21:10:38 +0900

    redirのtrim　途中

--- expansion/make_redir_list.c
+++ expansion/make_redir_list.c
@@ -6,7 +6,7 @@
 /*   By: tozeki <tozeki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 20:56:07 by toshi             #+#    #+#             */
-/*   Updated: 2024/04/26 19:13:23 by tozeki           ###   ########.fr       */
+/*   Updated: 2024/04/28 21:10:07 by tozeki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,89 @@ static void	add_redir_last(t_redir **head, t_redir *new)
 	find_last_redir(*head)->next = new;
 }
 
+static	t_token *find_new_last(t_token *redir, t_token *last, enum e_token_kind kind)
+{
+	t_token *ptr;
+	t_token *last_ptr;
+
+	last_ptr = NULL;
+	ptr = redir->next;
+	while (ptr != last)
+	{
+		if (ptr->kind != kind && ptr->next && ptr->next->kind == kind)
+			last_ptr = ptr;
+		ptr = ptr->next;
+	}
+	return (last_ptr);
+	//if (!last_ptr)
+	//	return ;
+	//ptr = last_ptr;
+	//while (ptr != last)
+	//{
+	//	remove
+	//}
+}
+
+static	t_token *find_new_last(t_token *redir, t_token *last, enum e_token_kind kind)
+{
+	t_token *ptr;
+	t_token *next;
+
+	ptr = redir->next;
+	while (ptr->kind == TKN_SPACE)
+		ptr = ptr->next;
+	return (ptr);
+}
+
+static	t_token *trim_tokens(t_token *first, t_token *last, enum e_token_kind kind)
+{
+	t_token *next_ptr;
+	t_token *last_ptr;
+	t_token *ptr;
+
+	ptr = first->next;
+	while (ptr != last && ptr->kind == kind)
+	{
+		next_ptr = ptr->next;
+		remove_token(&first, ptr, search_prev_token(first, ptr));
+		ptr = next_ptr;
+	}
+	while (ptr != last)
+	{
+		if (ptr->kind != kind && ptr->next && ptr->next->kind == kind)
+			last_ptr = ptr;
+		ptr = ptr->next;
+	}
+	
+}
+
+//first(redidr_tkn)の次には必ず何かのトークンが入っているのが前提で作成
+static void	trim_and_compress_space(t_token *redir, t_token *last)
+{
+	t_token *next_redir_first;
+	t_token *new_last;
+	t_token *new_first;
+	t_token *ptr;
+	t_token *prev;
+	t_token *next;
+
+	next_redir_first = last->next;
+	new_first = find_new_first();
+	new_last = find_new_last(redir, last, TKN_SPACE);
+	ptr = redir->next;
+	prev = redir;
+	while (ptr != new_first)
+	{
+		next = ptr->next;
+		remove_token(redir, ptr, prev);
+		prev = ptr;
+		ptr = next;
+	}
+	ptr = new_last
+
+
+}
+
 //リダイレクトのvalがifsのみだと、空文字列("")が入る
 static t_redir	*make_new_redir(t_token *first, t_token *last)
 {
@@ -39,6 +122,7 @@ static t_redir	*make_new_redir(t_token *first, t_token *last)
 
 	node = (t_redir *)ft_xmalloc(sizeof(t_redir));
 	node->kind = convert_redir_kind(first);
+	trim_and_compress_space(&first);
 	tmp_val = substr_from_tkn(first->next, last);
 	node->val = ft_xstrtrim(tmp_val, " \t\n");
 	free(tmp_val);
